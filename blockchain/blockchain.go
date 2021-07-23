52ea7b959f96c9d055d3155e3fb948f8720ea1c0
abc7468 <abc7468@naver.com>
2021-07-24 03:29:50 +0900

    refactoring blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -14,6 +14,7 @@ type block struct {
 	PrevHash string
 }
 
+// block의 주소를 저장하는 배열
 type blockchain struct {
 	blocks []*block
 }
@@ -21,11 +22,13 @@ type blockchain struct {
 var b *blockchain
 var once sync.Once
 
+//가진 데이터와 이전의 hash를 합한 새로운 hash 생성
 func (b *block) calculateHash() {
 	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
 	b.Hash = fmt.Sprintf("%x", hash)
 }
 
+// blockchain의 마지막 hash값 return
 func getLastHash() string {
 	totalBlocks := len(GetBlockchain().blocks)
 	if totalBlocks == 0 {
@@ -34,17 +37,20 @@ func getLastHash() string {
 	return GetBlockchain().blocks[totalBlocks-1].Hash
 }
 
+// block생성 후 생성된 block의 주솟값 return
 func createBlock(data string) *block {
 	newBlock := block{Data: data, Hash: "", PrevHash: getLastHash()}
 	newBlock.calculateHash()
 	return &newBlock
 }
 
+// createBlock함수로 생성된 block을 blockchain에 추가
 func (b *blockchain) AddBlock(data string) {
 	b.blocks = append(b.blocks, createBlock(data))
 
 }
 
+// blockchain instance를 가지고 오는 함수
 func GetBlockchain() *blockchain {
 	if b == nil {
 		//병렬적으로 처리한다고 해도 딱 한번만 실행하도록 도와주는 함수
@@ -56,6 +62,7 @@ func GetBlockchain() *blockchain {
 	return b
 }
 
+//blockchain이 가지고있는 block의 주솟값을 가진 list를 return
 func (b *blockchain) AllBlocks() []*block {
 	return b.blocks
 }
