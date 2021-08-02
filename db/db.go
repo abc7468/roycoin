146ed73c5895d0ae88a1bae79c6a1089c28edb9a
abc7468 <abc7468@naver.com>
2021-08-02 19:13:10 +0900

    bolt DB Update

---
+++ db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"github.com/abc7468/roycoin/utils"
+	"github.com/boltdb/bolt"
+)
+
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open("blockchain.db", 0600, nil)
+		db = dbPointer
+		utils.HandleErr(err)
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+	}
+	return db
+}
